8620e2e20fb7fbed826dc5a4324a8ca4041b783d
Ayush <ayushmishra2854@gmail.com>
2022-07-14 00:34:47 +0530

    basic contact page done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
 import './App.css';
+import ContactUs from './Pages/ContactUs/ContactUs';
 import FeatureEvents from './Pages/FeatureEvents/FeatureEvents';
 import OurPartners from './Pages/OurPartners/OurPartners';
 
 function App() {
   return (
     <div className="App">
-    {/* <h1>
-      Gdg Cloud kolkata Website
-    </h1> */}
-    <div className="container">
-      <FeatureEvents />
-    </div>
-    <div className="container">
-      <OurPartners />
-    </div>
+      {/* <h1>
+        Gdg Cloud kolkata Website
+      </h1> */}
+      <div className="container">
+        <FeatureEvents />
+      </div>
+      <div className="container">
+        <OurPartners />
+      </div>
+      <div className="container">
+        <ContactUs />
+      </div>
     </div>
   );
 }
